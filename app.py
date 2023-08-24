cbc628bc1b020dfa80e7d9a743a74487f5646f90
Yerin Ryu <dmsquf728@hanyang.ac.kr>
2023-08-25 01:26:20 +0900

    front ver-1 8/25

--- app.py
+++ app.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/detect')
+def detect():
+    return render_template('detect.html')
+
+@app.route('/select')
+def select():
+    return render_template('select.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
